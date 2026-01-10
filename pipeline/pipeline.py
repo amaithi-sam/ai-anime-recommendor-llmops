486557408ff7d350aff2899b87352fad8fb0b51c
amaithi-sam <amaithichirasan.s@gmail.com>
2026-01-11 00:40:57 +0530

    pipeling, prompt, recommender

---
+++ pipeline/pipeline.py
@@ -0,0 +1,41 @@
+from src.vector_store import VectorStoreBuilder
+from src.recommender import AnimeRecommender 
+from config.config import GROQ_API_KEY, MODEL_NAME
+
+from utils.logger import get_logger
+from utils.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+class AnimeRecommendationPipeline:
+
+    def __init__(self, persist_dir = 'chroma_db'):
+        
+        try:
+            logger.info("Initiating recommender pipeline")
+
+            vector_builder = VectorStoreBuilder(csv_path="", persis_dir=persist_dir)
+
+            retriever = vector_builder.load_vector_store().as_retriever()
+
+            self.recommender = AnimeRecommendationPipeline(retriever, GROQ_API_KEY, MODEL_NAME)
+
+            logger.info("pipeline initialized successfully")
+
+        except Exception as e:
+            logger.error("failed to initialize pipeline ", str(e))
+            raise CustomException("error during pipeline initializatrion", e)
+        
+    def recommend(self, query:str):
+        try:
+            logger.info(f"received a query {query}")
+
+            recommendation = self.recommender.get_recommendation(query)
+
+            logger.info("Recommendation generated Succesfully")
+
+            return recommendation 
+        except Exception as e:
+            logger.error(f"Error occured at generation/ recommendation {str(e)}")
+            raise CustomException(f"Error occured in generation/ recommendation ", e) 
+        
---
+++ src/recommender.py
@@ -0,0 +1,35 @@
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_groq import ChatGroq 
+from src.prompt_template import get_anime_prompt 
+import os 
+
+
+class AnimeRecommender:
+
+    def __init__(self, retriever, api_key:str, model_name:str):
+
+        self.llm = ChatGroq(api_key=api_key, model=model_name, temperature = 0.3)
+        self.prompt = get_anime_prompt()
+
+        # self.qa_chain = RetrievalQA.from_chain_type(
+        #     llm = self.llm,
+        #     chain_type = "stuff",
+        #     retriever = retriever,
+        #     return_source_documents = True,
+        #     chain_type_kwargs = {"prompt": self.prompt}
+        # )
+        combine_docs_chain = create_stuff_documents_chain(
+    llm=self.llm,
+    prompt=self.prompt
+)
+        self.qa_chain = create_retrieval_chain(
+    retriever=retriever,
+    combine_docs_chain=combine_docs_chain)
+        
+
+    def get_recommendation(self, query:str):
+        result = self.qa_chain({'query': query})
+
+        return result['result']
+
